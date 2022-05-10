54dc503e71337d568d03a2c7045e3a966293438f
Lukasz Zaczyk <zaczyk.l@gmail.com>
2022-05-10 12:41:08 +0200

    Add project files.

---
+++ ReportServiceViaSms/ReportService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ReportServiceViaSms
+{
+    public partial class ReportService : ServiceBase
+    {
+        private const int msInMinute=60000;
+        private readonly int _intervalInMinutes;
+        private Timer _timer=new Timer(msInMinute);
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        SmsSender _smsSender = new SmsSender(new SmsParams 
+        { 
+          AuthToken= ConfigurationManager.AppSettings["AuthToken"],
+          ReceiverTelNumber= ConfigurationManager.AppSettings["ReceiverTelNumber"],
+          SenderName = ConfigurationManager.AppSettings["SenderName"],
+          TextMessage ="Test message"
+        });
+        
+
+        public ReportService()
+        {
+            InitializeComponent();
+            _intervalInMinutes = 1 * msInMinute;
+            _timer = new Timer(_intervalInMinutes);
+        }
+
+        protected override void OnStart(string[] args)
+        {            
+            _timer.Elapsed += DoWork;
+            _timer.Start();
+            Logger.Info("Service started...");
+        }
+
+        private void DoWork(object sender, ElapsedEventArgs e)
+        {
+            Logger.Info("Wysyłam SMS.");
+            _smsSender.Send();
+            Logger.Info("Wysłano SMS");
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Info("Service stopped.");
+        }
+    }
+}
---
+++ ReportServiceViaSms/SmsSender.cs
@@ -0,0 +1,111 @@
+﻿using SMSApi.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportServiceViaSms
+{
+    public class SmsSender
+    {       
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private IClient _client;
+        private string _authToken;
+        private string _textMessage;
+        private string _receiverTelNumber;
+        private string _senderName;
+        public SmsSender(SmsParams smsParams)
+        {
+            try
+            {
+                _authToken= smsParams.AuthToken;
+                _textMessage= smsParams.TextMessage;
+                _receiverTelNumber= smsParams.ReceiverTelNumber;
+                _senderName= smsParams.SenderName;
+                _client = new ClientOAuth(_authToken);
+            }
+            catch (SMSApi.Api.Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
+        }
+
+        public void Send()
+        {
+            try
+            {
+                var smsApi = new SMSFactory(_client);
+
+                var result =
+                    smsApi.ActionSend()
+                        .SetText(_textMessage)
+                        .SetTo(_receiverTelNumber)
+                        .SetSender(_senderName) //Sender name
+                        .Execute();
+
+                Console.WriteLine("Send: " + result.Count);
+
+                string[] ids = new string[result.Count];
+
+                for (int i = 0, l = 0; i < result.List.Count; i++)
+                {
+                    if (!result.List[i].isError())
+                    {
+                        if (!result.List[i].isFinal())
+                        {
+                            ids[l] = result.List[i].ID;
+                            l++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Get:");
+                result =
+                    smsApi.ActionGet()
+                        .Ids(ids)
+                        .Execute();
+
+                foreach (var status in result.List)
+                {
+                    Console.WriteLine("ID: " + status.ID + " NUmber: " + status.Number + " Points:" + status.Points + " Status:" + status.Status + " IDx: " + status.IDx);
+                }
+            }
+            catch (ActionException e)
+            {
+                /**
+				 * Action error
+				 */
+                Logger.Error(e.Message);
+                Console.WriteLine(e.Message);
+            }
+            catch (ClientException e)
+            {
+                /**
+				 * Error codes (list available in smsapi docs). Example:
+				 * 101 	Invalid authorization info
+				 * 102 	Invalid username or password
+				 * 103 	Insufficient credits on Your account
+				 * 104 	No such template
+				 * 105 	Wrong IP address (for IP filter turned on)
+				 * 110	Action not allowed for your account
+				 */
+                Logger.Error(e.Message);
+                System.Console.WriteLine(e.Message);
+            }
+            catch (HostException e)
+            {
+                /* 
+				 * Server errors
+				 * SMSApi.Api.HostException.E_JSON_DECODE - problem with parsing data
+				 */
+                Logger.Error(e.Message);
+            }
+            catch (ProxyException e)
+            {
+                // communication problem between client and sever
+                Logger.Error(e.Message);
+            }
+        }
+    }
+}
